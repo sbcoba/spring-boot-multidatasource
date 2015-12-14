cf138f136f7ee68b2ec6aad15dfbd21b627780d1
preston.sbcoba <sbcoba@gmail.com>
2015-12-14 18:56:30 +0900

    DataSource 명 변경된 부분에서 빠진 부분 변경2

--- spring-boot-multidatasource-sample/spring-boot-multidatasource-sample-default/src/main/java/org/sbcoba/springboot/multidatasource/sample/service/TestService.java
+++ spring-boot-multidatasource-sample/spring-boot-multidatasource-sample-default/src/main/java/org/sbcoba/springboot/multidatasource/sample/service/TestService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 /**
  * 테스트에 사용할 인터페이스
+ *
  * @author sbcoba
  */
 public interface TestService {
--- spring-boot-multidatasource-sample/spring-boot-multidatasource-sample-default/src/main/java/org/sbcoba/springboot/multidatasource/sample/service/TestServiceByCustomAnnotationImpl.java
+++ spring-boot-multidatasource-sample/spring-boot-multidatasource-sample-default/src/main/java/org/sbcoba/springboot/multidatasource/sample/service/TestServiceByCustomAnnotationImpl.java
@@ -1,9 +1,9 @@
 package org.sbcoba.springboot.multidatasource.sample.service;
 
-import org.sbcoba.springboot.multidatasource.sample.annotation.DevDataSource;
-import org.sbcoba.springboot.multidatasource.sample.annotation.ProdDataSource;
-import org.sbcoba.springboot.multidatasource.sample.annotation.StagingDataSource;
-import org.sbcoba.springboot.multidatasource.sample.annotation.TestDataSource;
+import org.sbcoba.springboot.multidatasource.sample.annotation.Db1DataSource;
+import org.sbcoba.springboot.multidatasource.sample.annotation.Db4DataSource;
+import org.sbcoba.springboot.multidatasource.sample.annotation.Db3DataSource;
+import org.sbcoba.springboot.multidatasource.sample.annotation.Db2DataSource;
 import org.sbcoba.springboot.multidatasource.sample.repository.TestDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -15,10 +15,10 @@ import java.util.List;
 /**
  * 커스터 어노테이션으로 @DataSource 랩핑한 형태로 가능
  *
- * @see DevDataSource
- * @see TestDataSource
- * @see StagingDataSource
- * @see ProdDataSource
+ * @see Db1DataSource
+ * @see Db2DataSource
+ * @see Db3DataSource
+ * @see Db4DataSource
  * @author sbcoba
  *
  */
@@ -30,28 +30,28 @@ public class TestServiceByCustomAnnotationImpl implements TestService {
     private TestDao testDao;
 
     @Override
-    @DevDataSource
+    @Db1DataSource
     @Transactional(readOnly = true)
     public List<String> db1Names() {
         return testDao.getNames();
     }
 
     @Override
-    @TestDataSource
+    @Db2DataSource
     @Transactional(readOnly = true)
     public List<String> db2Names() {
         return testDao.getNames();
     }
 
     @Override
-    @StagingDataSource
+    @Db3DataSource
     @Transactional(readOnly = true)
     public List<String> db3Names() {
         return testDao.getNames();
     }
 
     @Override
-    @ProdDataSource
+    @Db4DataSource
     @Transactional(readOnly = true)
     public List<String> db4Names() {
         return testDao.getNames();
